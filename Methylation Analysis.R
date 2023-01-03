21ac4d2f411c3d4feb8c049cad766d72a6f9642a
Arsham Mikaeili Namini <92790984+Arshammik@users.noreply.github.com>
2023-01-04 03:04:06 +0330

    Adding Document tips to code

--- Methylation Analysis.R
+++ Methylation Analysis.R
@@ -19,14 +19,14 @@ Sample_Group <- c((rep("tumor", 34)), (rep("normal", 14 )))
 GSE77954 <- data.frame(CT, GSE, stringsAsFactors = F)
 GSE77954 <- data.frame(Sample_Name = sub("\\[\\w+\\]$", "", GSE77954$CT), Sample_Plate = GSE, Sample_Group = Sample_Group, stringsAsFactors = F)
 
-csv_file <- data.frame(Sample_Name = GSE77954$Sample_Name,
-                       Sample_Plate = NA ,
+PD <- data.frame(Sample_Name = GSE77954$Sample_Name,
+                       Sample_Plate = GSE77954$Sample_Plate ,
                        Sample_Group = GSE77954$Sample_Group,
                        Pool_ID = NA,
                        Project = NA,
                        Sample_Well = NA,
-                       Sentrix_ID = Sentix_ID$Sentix_ID,
-                       Sentrix_Position = Sentix_ID$Sentix_Position)
+                       Slide(Sentrix_ID) = Sentix_ID$Sentix_ID,
+                       Array(Sentrix_Position) = Sentix_ID$Sentix_Position)
 write.csv(csv_file, "~/desktop/Methylation Analysis/Data/GSE77954_RAW/csv_file.csv")
 
 # Start the analysis
@@ -34,15 +34,19 @@ install.packages("ChAMP") #requiemnet
 library(ChAMP)
 
 setwd("~/desktop/Methylation Analysis/")
-myLoad <- champ.load("~/desktop/Methylation Analysis/Data/GSE77954_RAW/")
-
-CpG.GUI(CpG=rownames(myLoad$beta),arraytype="450K")
-
-champ.QC()  # Alternatively: QC.GUI()
+myImport <- champ.load("~/desktop/Methylation Analysis/Data/GSE77954_RAW/")
+myLoad <- champ.filter()
 
 
-myNorm <- champ.norm()
-champ.SVD()
+#This is a useful function to demonstrate the distribution of your CpG list.
+#If you get a DMP list, you may use this function to check your DMP’s distribution on chromosome.
+
+CpG.GUI()
+QC.GUI(beta=myLoad$beta,arraytype="450K") # We have diffrent results because `champ.filter()` function
+
+
+myNorm <- champ.norm(beta=myLoad$beta,arraytype="450K",cores=5)
+champ.SVD(beta=myNorm,pd=myLoad$pd)
 
 myDMP  <- champ.DMP()
 DMP.GUI()
